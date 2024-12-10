78c792c952333b9464b3518abaa72791bd36bf13
Carballo Otero <macarballootero@deloitte.es>
2024-12-10 08:28:35 +0100

    La data de dias anteriores se guarda en la carpeta 'historic' con el nombre formateado con la fecha, ajustes en el main para albergar ambas funcionalidades.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import os
+import shutil
+import datetime
 from parser import cleanse_data, parse_data_to_dataframe
 from scrapper import data_scrapper
 
-# Se escrapean los datos, se limpian y se anhaden a un archivo csv
-data_scrapper()
-clean_data = cleanse_data()
-df = parse_data_to_dataframe(clean_data)
-df.to_csv("output/spain_stocks.csv")
+# Si la carpeta output ya posee data la mueve a la carpeta de historico
+if os.path.exists('output/spain_stocks.csv'):
+    date = datetime.date.today()
+    shutil.move('output/spain_stocks.csv', f'historic/spain_stocks_{date}.csv')
+
+else: # Si no posee data, se escrapean los datos, se limpian y se incluyen en un archivo csv
+    data_scrapper()
+    clean_data = cleanse_data()
+    df = parse_data_to_dataframe(clean_data)
+    df.to_csv("output/spain_stocks.csv")
